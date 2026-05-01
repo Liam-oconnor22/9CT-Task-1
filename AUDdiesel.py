5f1027efaa40535dc422209701e91c320ba3f543
Liam-oconnor22 <liam.oconnor22@education.nsw.gov.au>
2026-05-01 12:30:13 +1000

    fixed file

--- AUDdiesel.py
+++ AUDdiesel.py
@@ -2,4 +2,5 @@ import pandas as pd
 import time
 import sys
 
-AUD_diesel = pd.read_csv('data/Avg_DieselPrice_AUD.csv', on_bad_lines="skip")
+AUD_diesel = pd.read_csv('Avg_DieselPrice_AUD.csv', on_bad_lines="skip")
+print(AUD_diesel)
